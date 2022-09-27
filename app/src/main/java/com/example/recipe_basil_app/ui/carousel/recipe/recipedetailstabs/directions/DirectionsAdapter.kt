26d73a8b7e783484c428aa7d560ce2ad965d13d7
Ricardo Taipe <rdavid28@hotmail.es>
2022-09-27 17:59:41 -0500

    add directions fragment

--- app/src/main/java/com/example/recipe_basil_app/ui/carousel/recipe/recipedetailstabs/directions/DirectionsAdapter.kt
+++ app/src/main/java/com/example/recipe_basil_app/ui/carousel/recipe/recipedetailstabs/directions/DirectionsAdapter.kt
@@ -1,34 +1,80 @@
 package com.example.recipe_basil_app.ui.carousel.recipe.recipedetailstabs.directions
 
+import android.graphics.Typeface
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recipe_basil_app.databinding.DirectionItemBinding
+import com.example.recipe_basil_app.databinding.IndicatorItemBinding
 import com.example.recipe_basil_app.network.response.DirectionModel
 
-class DirectionsAdapter :
-    ListAdapter<DirectionModel, DirectionsAdapter.ViewHolder>(DirectionModelDiffCallback) {
+const val LAYOUT_DIRECTION = 0
+const val LAYOUT_INDICATOR = 1
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val binding = DirectionItemBinding.inflate(layoutInflater, parent, false)
-        return ViewHolder(binding)
+class DirectionsAdapter(private val viewType: Int) :
+    ListAdapter<DirectionModel, RecyclerView.ViewHolder>(DirectionModelDiffCallback) {
+    var selectedPos = RecyclerView.NO_POSITION
+
+    var listener: ((Int, View) -> Unit)? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == LAYOUT_DIRECTION) {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val binding = DirectionItemBinding.inflate(layoutInflater, parent, false)
+            ViewHolderDirection(binding)
+        } else {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val binding = IndicatorItemBinding.inflate(layoutInflater, parent, false)
+            ViewHolderIndicator(binding)
+        }
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(getItem(position))
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (viewType == LAYOUT_DIRECTION) {
+            (holder as ViewHolderDirection).bind(getItem(position))
+        } else {
+            (holder as ViewHolderIndicator).bind(getItem(position), listener, selectedPos)
+        }
     }
 
-    class ViewHolder(private val binding: DirectionItemBinding) :
+    override fun getItemViewType(position: Int): Int {
+        return viewType
+    }
+
+
+    class ViewHolderDirection(private val binding: DirectionItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(directionModel: DirectionModel) {
 
             binding.apply {
                 direction = directionModel
-                //descriptionText = directionModel.description
+                executePendingBindings()
+            }
+        }
+    }
+
+    class ViewHolderIndicator(private val binding: IndicatorItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(directionModel: DirectionModel, listener: ((Int, View) -> Unit)?, selectedPos: Int) {
+            val isSelected = selectedPos == adapterPosition
+            binding.apply {
+                root.setOnClickListener {
+                    listener?.invoke(directionModel.id, it)
+                }
+                indicatorText.apply {
+                    text = String.format("%02d", directionModel.id + 1)
+                    setTypeface(
+                        this.typeface,
+                        if (isSelected) Typeface.BOLD else Typeface.NORMAL
+                    )
+                    scaleX = if (isSelected) 1.75f else 1f
+                    scaleY = if (isSelected) 1.75f else 1f
+                }
                 executePendingBindings()
             }
         }
--- app/src/main/java/com/example/recipe_basil_app/ui/carousel/recipe/recipedetailstabs/directions/DirectionsFragment.kt
+++ app/src/main/java/com/example/recipe_basil_app/ui/carousel/recipe/recipedetailstabs/directions/DirectionsFragment.kt
@@ -1,11 +1,14 @@
 package com.example.recipe_basil_app.ui.carousel.recipe.recipedetailstabs.directions
 
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.example.recipe_basil_app.databinding.FragmentDirectionsBinding
 import com.example.recipe_basil_app.ui.carousel.recipe.recipedetailstabs.RecipeDetailsTabsViewModel
 
@@ -13,7 +16,10 @@ import com.example.recipe_basil_app.ui.carousel.recipe.recipedetailstabs.RecipeD
 class DirectionsFragment : Fragment() {
     private lateinit var binding: FragmentDirectionsBinding
     private val viewModel: RecipeDetailsTabsViewModel by viewModels({ requireParentFragment() })
-    private val adapter = DirectionsAdapter()
+    private val adapter = DirectionsAdapter(LAYOUT_DIRECTION)
+    private val indicatorAdapter = DirectionsAdapter(LAYOUT_INDICATOR)
+    private lateinit var callback: OnPageChangeCallback
+    private val snapHelper = LinearSnapHelper()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,13 +31,47 @@ class DirectionsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.directionsList.adapter = adapter
-
         viewModel.recipe.observe(viewLifecycleOwner) {
             it?.let { recipe ->
                 adapter.submitList(recipe.getDirections())
+                indicatorAdapter.submitList(recipe.getDirections())
             }
         }
+
+        binding.directionsList.adapter = adapter
+        binding.directionsList.setPageTransformer { page, position ->
+            if (position <= 0) {
+                page.alpha = 1f + position
+            }
+            if (position <= 1) {
+                page.alpha = 1f - position
+            }
+        }
+        binding.pagerIndicator.adapter = indicatorAdapter
+        callback = object :
+            OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                indicatorAdapter.selectedPos = position
+                indicatorAdapter.notifyDataSetChanged()
+                binding.pagerIndicator.smoothScrollToPosition(position)
+            }
+        }
+
+        binding.directionsList.registerOnPageChangeCallback(callback)
+        snapHelper.attachToRecyclerView(binding.pagerIndicator)
+
+
+        indicatorAdapter.listener = { position, _ ->
+            indicatorAdapter.selectedPos = position
+            indicatorAdapter.notifyDataSetChanged()
+            binding.directionsList.setCurrentItem(position, true)
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.directionsList.unregisterOnPageChangeCallback(callback)
     }
 }
